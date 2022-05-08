94bbc34a687b3d86c371df2c476c31847f74c6cc
sunboxsoft <sunboxsoft@163.com>
2022-05-08 12:55:24 +0800

    axios

--- src/components/AxiosTestFullPost.vue
+++ src/components/AxiosTestFullPost.vue
@@ -1,38 +1,32 @@
 <template>
-  <section class="AxiosTestFullPost">
-    
-  </section>
+  <section class="AxiosTestFullPost"></section>
 </template>
 
 <script>
 import axios from "axios";
 export default {
- name: 'AxiosTestFullPost',
+  name: "AxiosTestFullPost",
   data() {
-    return {
-
-    }
+    return {};
   },
   components: {},
   watch: {},
-  created(){
-      let result = axios({
+  created() {
+    let result = axios({
       method: "POST",
       url: "http://www.liulongbin.top:3006/api/post",
-      data:{
-          name:'zs',
-          age:20
+      data: {
+        name: "zs",
+        age: 20
       }
     });
-    result.then((res) => {
+    result.then(res => {
       console.log(res);
     });
   },
   mounted() {},
   methods: {}
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
