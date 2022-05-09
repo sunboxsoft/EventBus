09fc13d1e35f0e223e57bedf59f3329d2e64c6cb
sunboxsoft <sunboxsoft@163.com>
2022-05-09 13:06:53 +0800

    axios post 传参的两种格式

--- src/components/AxiosTestFullPost.vue
+++ src/components/AxiosTestFullPost.vue
@@ -4,6 +4,7 @@
 
 <script>
 import axios from "axios";
+import qs from 'qs'
 export default {
   name: "AxiosTestFullPost",
   data() {
@@ -15,10 +16,15 @@ export default {
     let result = axios({
       method: "POST",
       url: "http://www.liulongbin.top:3006/api/post",
+      //headers: {Accept: 'application/json, text/plain, */*', Content-Type: 'application/json'}
+      //data: "{\"name\":\"zs\",\"age\":20}"
       data: {
         name: "zs",
         age: 20
       }
+
+      // headers: {Accept: 'application/json, text/plain, */*', Content-Type: 'application/x-www-form-urlencoded'}
+      // data:qs.stringify({name: "zs",age: 20})  //data: "name=zs&age=20"
     });
     result.then(res => {
       console.log(res);
--- src/components/AxiosTestPost.vue
+++ src/components/AxiosTestPost.vue
@@ -6,6 +6,7 @@
 
 <script>
 import axios from 'axios'
+import qs from 'qs'
 export default {
  name: 'AxiosTestPost',
   data() {
@@ -15,16 +16,36 @@ export default {
   },
   components: {},
   watch: {},
+
+//Content-Type: application/json; charset=utf-8
 // created(){
 //     axios.post('http://www.liulongbin.top:3006/api/post',{name:'zs',gender:'女'}).then(result=>{
 //         const {data:res}= result
 //         console.log(result,res,'const {data:res}= res')
 //     })
 // },
+
+//Content-Type: application/json; charset=utf-8
  async created(){
     let {data:res}=await  axios.post('http://www.liulongbin.top:3006/api/post',{name:'zs',gender:'女'})
     console.log(res,' async created(){')
   },
+
+//  Accept: application/json, text/plain, */*
+//  async created(){
+//     let data = new URLSearchParams()
+//     data.append('name','zs')
+//     data.append('gender','女1')
+//     let {data:res}=await  axios.post('http://www.liulongbin.top:3006/api/post',data)
+//     console.log(res,' async created(){')
+//   },
+
+ //Content-Type: application/json; charset=utf-8
+//  async created(){
+//     console.log(qs.stringify({name:'zs',gender:'女1qs11'}))
+//     let {data:res}=await  axios.post('http://www.liulongbin.top:3006/api/post',qs.stringify({name:'zs',gender:'女1qs11'}))
+//     console.log(res,' async created(){')
+//   },
   mounted() {},
   methods: {}
 }
